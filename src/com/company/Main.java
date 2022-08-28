a7118e4cf1beb9c42685b58a59bb84067cbac9c5
josebasgil <josebasgil29@gmail.com>
2022-08-28 09:53:46 -0500

    planet 1 added and error free

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,12 +12,20 @@ public class Main {
         System.out.println("the basic structure is ready ");
 
 
-//    developing the feature SUN
+    // developing the feature SUN
         SUN omaro=new SUN();
         omaro.temperature="12334433kj";
         omaro.name="ferro";
         omaro.radius=1234455;
         omaro.color="pink";
+
+    // developing planet 1
+        Planet1 superplanet=new Planet1();
+        superplanet.color="magenta";
+        superplanet.name="hash";
+        superplanet.size="545488km";
+
+
     }
 
 
